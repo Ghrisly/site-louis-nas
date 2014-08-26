a16d03448f474ee20fa3049e2182646717627427
Maïté Amourdon <maite.amourdon@gmail.com>
2014-08-26 18:17:09 +0200

    Rectification bandeau Menu Widget faebook de l'accueil

--- js/app.js
+++ js/app.js
@@ -6,14 +6,30 @@ var app = angular.module('app', [
   'ngAnimate',
   'ngResource',
   'ngRoute',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ui.bootstrap.dropdown'
 ]);
 
-app.config(function($routeProvider) {
-    $routeProvider.when('/',
-      {
-        templateUrl: "partials/accueil.html",
-        controller: "HomeCtrl"
-      }
-    )
-});
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+        when('/',
+          {
+            templateUrl: "partials/accueil.html",
+            controller: "HomeCtrl"
+          }
+        )
+        .when('/overview', 
+          {
+            templateUrl: "partials/accueil.html",
+            controller: "HomeCtrl"
+          }
+        )
+        .when('/users', 
+          {
+            templateUrl: "partials/accueil.html",
+            controller: "HomeCtrl"
+          }
+        )
+        .otherwise({redirectTo: '/'});
+    $locationProvider.html5Mode(true);
+}]);
