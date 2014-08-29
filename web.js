cb29535952438c91ac24b0538e31905e75073a2c
Maïté Amourdon <maite.amourdon@gmail.com>
2014-08-29 13:48:25 +0200

    gitignore

--- web.js
+++ web.js
@@ -1,19 +1,12 @@
 var gzippo = require('gzippo');
 var express = require('express');
 
-var cool = require('cool-ascii-faces')
-
-
-
-
 
 var port = process.env.PORT || 5000;
 var app = express();
  
 app.use(express.logger());
-app.get('/', function(request, response) {
-  response.send(cool());
-});
+
 
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(port, function() {
